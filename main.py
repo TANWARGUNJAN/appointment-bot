e4845884b8995da0de105e2e1cb4aca9e65e81af
TANWARGUNJAN <tanwargunj45@gmail.com>
2025-06-27 23:12:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
+from backend import calendar_utils
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Appointment Booking API is running!"}
+
+@app.get("/free-slots")
+def get_slots():
+    return {"available_slots": calendar_utils.get_free_slots()}
+
+@app.post("/book")
+def book():
+    return {"status": calendar_utils.book_slot()}
 
